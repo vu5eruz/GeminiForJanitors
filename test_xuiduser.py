5c7043ffd75fd1d4d4aafa70bbc444a7baa4fa44
vu5eruz <undefined@disroot.org>
2025-08-02 21:12:26 -0400

    Initial xuiduser module

---
+++ test_xuiduser.py
@@ -0,0 +1,125 @@
+import pytest
+import secrets
+
+from xuiduser import XUID, LocalUserStorage
+
+################################################################################
+
+def test_xuid():
+    """Basic object construction and comparison tests."""
+
+    salt_a = secrets.token_bytes(32)
+    salt_b = secrets.token_bytes(32)
+
+    user_a = 'api-key-a'
+    user_b = 'api-key-b'
+
+    # same user same salt should be equal
+    assert XUID(user_a, salt_a) == XUID(user_a, salt_a)
+    assert XUID(user_b, salt_b) == XUID(user_b, salt_b)
+
+    # same user different salt should NOT be equal
+    assert XUID(user_a, salt_a) != XUID(user_a, salt_b)
+    assert XUID(user_b, salt_a) != XUID(user_b, salt_b)
+
+    # different user same salt should NOT be equal
+    assert XUID(user_a, salt_a) != XUID(user_b, salt_a)
+    assert XUID(user_a, salt_b) != XUID(user_b, salt_b)
+
+    # different user different salt should NOT be equal
+    assert XUID(user_a, salt_a) != XUID(user_b, salt_b)
+
+@pytest.mark.parametrize("other", [
+    None,
+    int(),
+    float(),
+    complex(),
+    bool(),
+    list(),
+    tuple(),
+    range(1),
+    str(),
+    bytes(),
+    bytearray(),
+    memoryview(b''),
+    set(),
+    frozenset(),
+    dict(),
+])
+def test_xuid_cmp(other):
+    """Defensive programming, a XUID must NOT be compared with other types."""
+
+    xuid = XUID("john smith", "The Quick Brown Fox Jumps Over The Lazy Dog")
+
+    with pytest.raises(TypeError):
+        _ = (xuid == other)
+
+    with pytest.raises(TypeError):
+        _ = (xuid != other)
+
+def test_xuid_len():
+    """Test whether XUID string lengths are exactly as advertised."""
+
+    xuid = XUID("john smith", "The Quick Brown Fox Jumps Over The Lazy Dog")
+
+    assert len(repr(xuid)) == XUID.LEN_REPR
+    assert len(str(xuid))  == XUID.LEN_STR
+
+################################################################################
+
+@pytest.mark.parametrize("storage", [
+    LocalUserStorage(),
+])
+def test_storage(storage):
+    """Basic storage tests."""
+
+    salt = secrets.token_bytes(32)
+
+    user_1 = XUID("user-1", salt)
+    user_2 = XUID("user-2", salt)
+    user_3 = XUID("user-3", salt)
+    user_4 = XUID("user-4", salt)
+
+    # Ensure that these users are not in the storage
+    assert storage.get(user_1) == ({}, False)
+    assert storage.get(user_2) == ({}, False)
+    assert storage.get(user_3) == ({}, False)
+    assert storage.get(user_4) == ({}, False)
+
+    # Store these users and check that they weren't already in the storage
+    assert not storage.put(user_1, { 'abc': 'def' })
+    assert not storage.put(user_2, { 'abc': 'def' })
+    assert not storage.put(user_3, { 'abc': 'def' })
+    assert not storage.put(user_4, { 'abc': 'def' })
+
+    # Update users' data and check that they are still present in the storage
+    assert storage.put(user_1, { 'abc': 123 })
+    assert storage.put(user_2, { 'abc': 123 })
+    assert storage.put(user_3, { 'abc': 123 })
+    assert storage.put(user_4, { 'abc': 123 })
+
+    # Check that the users are in the storage and their data is stored correctly
+    assert storage.get(user_1) == ({ 'abc': 123 }, True)
+    assert storage.get(user_2) == ({ 'abc': 123 }, True)
+    assert storage.get(user_3) == ({ 'abc': 123 }, True)
+    assert storage.get(user_4) == ({ 'abc': 123 }, True)
+
+    # Clean up and check that the storage was cleaned up correctly
+    storage.rem(user_1)
+    storage.rem(user_2)
+    storage.rem(user_3)
+    storage.rem(user_4)
+
+    with pytest.raises(KeyError):
+        storage.rem(user_1)
+
+    with pytest.raises(KeyError):
+        storage.rem(user_2)
+
+    with pytest.raises(KeyError):
+        storage.rem(user_3)
+
+    with pytest.raises(KeyError):
+        storage.rem(user_4)
+
+################################################################################
---
+++ xuiduser.py
@@ -0,0 +1,137 @@
+"""X-Unique/User Identifier (XUID) and User Settings/Storage.
+
+XUIDs are a mechanism to anonymously identify unique users from sensitive data.
+The proxy has no access to any user-unique data or info except their API keys.
+Hashing an API key with a secret salt gives an unique user ID for program use.
+"""
+
+# xuiduser
+
+from base64 import urlsafe_b64encode as _base64
+from colorama.ansi import Fore as _colorama_ansi_fore
+from hashlib import sha256 as _hash_fun # Choice of hash function is arbitrary
+from hmac import digest as _hmac_digest
+
+__all__ = ['XUID', 'UserSettings']
+
+_color_palette = [
+    # no black, it'd be unreadable on dark theme
+    _colorama_ansi_fore.RED,
+    _colorama_ansi_fore.GREEN,
+    _colorama_ansi_fore.YELLOW,
+    _colorama_ansi_fore.BLUE,
+    _colorama_ansi_fore.MAGENTA,
+    _colorama_ansi_fore.CYAN,
+    # no white, it'd be unreadable on light theme
+]
+
+_color_reset = _colorama_ansi_fore.RESET
+
+################################################################################
+
+class XUID:
+    """X-Unique/User Identifier.
+
+    :param user: The user's API key or any sensitive unique identifier.
+    :param salt: Program global secure random string.
+
+    Usage::
+
+      >>> import secrets
+      >>> import xuiduser
+      >>> XUID_SALT = secrets.token_bytes(32) # or os.environ["XUID_SALT"]
+      >>> user_api_key = "..." # provided to the proxy during a request
+      >>> xuid = xuiduser.XUID(user_api_key, XUID_SALT)
+    """
+
+    LEN_REPR = len("xuid:") + (4 * _hash_fun().digest_size + 2) // 3
+    LEN_STR  = 8 # Arbitrary, could be made a global proxy settings
+
+    def __init__(self, user: str | bytes, salt: str | bytes):
+        user = user.encode('utf-8') if isinstance(user, str) else user
+        salt = salt.encode('utf-8') if isinstance(salt, str) else salt
+
+        self._xuid_raw = _hmac_digest(salt, user, _hash_fun)
+        self._xuid_str = _base64(self._xuid_raw).rstrip(b'=').decode('ascii')
+
+    def __hash__(self) -> int:
+        return hash(self._xuid_raw)
+
+    def __repr__(self) -> str:
+        """Returns the full XUID value representation as a string.
+        This is safe to handle and can be used as an alternative XUID form."""
+        return f"xuid:{self._xuid_str}"
+
+    def __str__(self) -> str:
+        """Returns a shortened XUID value representation as a string.
+        This has a higher risk of collisions and should be handled with care."""
+        return self._xuid_str[:XUID.LEN_STR]
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, XUID):
+            # This SHOULD NOT happen, raise a hard and explicit error
+            raise TypeError(f"Can't compare a XUID with {type(other).__name__}")
+        return self._xuid_raw == other._xuid_raw
+
+    def __ne__(self, other) -> bool:
+        if not isinstance(other, XUID):
+            # This SHOULD NOT happen, raise a hard and explicit error
+            raise TypeError(f"Can't compare a XUID with {type(other).__name__}")
+        return self._xuid_raw != other._xuid_raw
+
+    def pretty(self) -> str:
+        """Returns a prettified, shortened XUID value string for printing.
+        This includes ANSI escape codes and is not meant for processing."""
+        color = _color_palette[hash(self) % len(_color_palette)]
+        return f"{color}<{self}>{_color_reset}"
+
+################################################################################
+
+class UserSettings:
+    pass # TODO
+
+################################################################################
+
+class UserStorage:
+    """Abstract base class for a key-value storage.
+
+    When a method return True, it means the given user exists in the storage.
+    A method return False to indicate that a user isn't or wasn't stored."""
+
+    def get(self, xuid: XUID) -> tuple[dict, bool]:
+        """Gets the user data if they exist in the storage."""
+        raise NotImplementedError("UserStorage.get")
+
+    def put(self, xuid: XUID, data: dict) -> bool:
+        """Puts the user data in the storage, overwriting any old data."""
+        raise NotImplementedError("UserStorage.put")
+
+    def rem(self, xuid: XUID):
+        """Removes the user and their data from the storage.
+        Raises a KeyError if the user is not present on the storage."""
+        raise NotImplementedError("UserStorage.rem")
+
+class LocalUserStorage(UserStorage):
+    """Implements a non-persistent in-memory storage."""
+
+    def __init__(self):
+        self._storage: dict[XUID, dict] = dict()
+
+    def get(self, xuid: XUID) -> tuple[dict, bool]:
+        data = self._storage.get(xuid)
+        if data is None:
+            return {}, False
+        return data, True
+
+    def put(self, xuid: XUID, data: dict) -> bool:
+        xuid_in_storage = xuid in self._storage
+        self._storage[xuid] = data
+        return xuid_in_storage
+
+    def rem(self, xuid: XUID):
+        del self._storage[xuid]
+
+class RedisUserStorage(UserStorage):
+    pass # TODO
+
+################################################################################
