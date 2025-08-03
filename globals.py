2ec720e45b96c409a6de662eb4317a409b1c5eb7
vu5eruz <undefined@disroot.org>
2025-08-02 21:41:25 -0400

    Initial globals module

---
+++ globals.py
@@ -0,0 +1,38 @@
+"""Proxy global variables."""
+
+
+# Based on https://github.com/numpy/numpy/blob/c6169853b871411ef207db1987a27f134a89b1cb/numpy/_build_utils/gitversion.py#L21
+def _append_git_version(version):
+    import os.path
+    import subprocess
+
+    try:
+        p = subprocess.Popen(
+            ["git", "log", "-1", '--format="%H"'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd=os.path.dirname(__file__),
+        )
+    except FileNotFoundError:
+        pass
+    else:
+        out, err = p.communicate()
+
+        if p.returncode == 0:
+            git_hash = out.decode("utf-8").strip()
+
+            if git_hash:
+                version += f"-{git_hash[:7]}"
+
+    return version
+
+
+################################################################################
+
+PROXY_AUTHORS = [
+    "@undefinedundefined (vu5eruz on GitHub)",
+]
+
+PROXY_VERSION = _append_git_version("2025.08.02")
+
+################################################################################
