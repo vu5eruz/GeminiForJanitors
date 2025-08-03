e4b5a28e9887e59c70b25109d5ebbaf035ddec11
vu5eruz <undefined@disroot.org>
2025-08-03 13:25:25 -0400

    Tweak __init__.py

--- gfjproxy/__init__.py
+++ gfjproxy/__init__.py
@@ -2,24 +2,13 @@
 
 from ._globals import PROXY_AUTHORS, PROXY_VERSION
 from .app import app
-from .xuiduser import (
-    XUID,
-    LocalUserStorage,
-    RedisUserStorage,
-    UserSettings,
-    UserStorage,
-)
 
 __all__ = [
     "app",
     "PROXY_AUTHORS",
     "PROXY_VERSION",
-    "XUID",
-    "LocalUserStorage",
-    "RedisUserStorage",
-    "UserSettings",
-    "UserStorage",
 ]
 
 __author__ = PROXY_AUTHORS
+
 __version__ = PROXY_VERSION
