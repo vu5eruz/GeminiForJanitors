fee2f0468d876eb8dd11ca3bf2b2f852df791664
vu5eruz <undefined@disroot.org>
2025-08-04 16:04:32 -0400

    Add ResponseHelper

--- gfjproxy/utils.py
+++ gfjproxy/utils.py
@@ -2,11 +2,182 @@
 
 import atexit
 import click
+import json
 import re
 import requests
 import subprocess
-import time
 import threading
+import time
+from dataclasses import dataclass
+from enum import Enum
+from flask import Response
+
+################################################################################
+
+
+class MessageKind(Enum):
+    CHAT = 0
+    ERROR = 1
+    PROXY = 2
+
+
+@dataclass(frozen=True, kw_only=True)
+class ResponseMessage:
+    """Response message model."""
+
+    kind: MessageKind
+
+    text: str
+
+    status_code: int = None
+
+
+class ResponseHelper:
+    """Response helper to provide JanitorAI with valid responses."""
+
+    def __init__(self, *, wrap_errors: bool = False):
+        self._messages = []
+        self._status = 200
+        self._wrap_errors = wrap_errors
+
+    def add_error(self, message, status_code: int):
+        self._messages.append(
+            ResponseMessage(
+                kind=MessageKind.ERROR,
+                text=str(message),
+                status_code=status_code,
+            )
+        )
+        self._status = status_code
+        return self
+
+    def add_message(self, *messages):
+        for message in messages:
+            self._messages.append(
+                ResponseMessage(kind=MessageKind.CHAT, text=str(message))
+            )
+        return self
+
+    def add_proxy_message(self, *messages):
+        for message in messages:
+            self._messages.append(
+                ResponseMessage(kind=MessageKind.PROXY, text=str(message))
+            )
+        return self
+
+    def build(self) -> Response:
+        if self._status != 200 and len(self._messages) == 1:
+            if self._wrap_errors:
+                return Response(
+                    response=[json.dumps({"error": self.message.strip()})],
+                    status=self._status,
+                    content_type="application/json; charset=utf-8",
+                )
+            else:
+                return Response(
+                    response=[self.message],
+                    status=self._status,
+                    content_type="text/plain; charset=utf-8",
+                )
+        else:  # self._status == 200 or complex multi-line message
+            return Response(
+                response=[
+                    json.dumps(
+                        {
+                            "choices": [
+                                {
+                                    "index": 0,
+                                    "message": {
+                                        "role": "assistant",
+                                        "content": self.message,
+                                    },
+                                    "finish_reason": "stop",
+                                }
+                            ]
+                        }
+                    )
+                ],
+                status=200,
+                content_type="application/json; charset=utf-8",
+            )
+
+    def build_error(self, message, status_code: int):
+        return self.add_error(message, status_code).build()
+
+    def build_message(self, *messages):
+        return self.add_message(*messages).build()
+
+    @property
+    def message(self) -> str:
+        if not self._messages:
+            return ""
+
+        if len(self._messages) == 1:
+            if self._messages[0].kind == MessageKind.CHAT:
+                return self._messages[0].text
+            if self._messages[0].kind == MessageKind.ERROR:
+                if self._wrap_errors:
+                    return f"PROXY ERROR {self._messages[0].status_code}: {self._messages[0].text}"
+                return self._messages[0].text
+            return f"<proxy>{self._messages[0].text}\n</proxy>"
+
+        last_msg = self._messages[0].kind
+        proxy_msg_group = last_msg == MessageKind.PROXY
+        result = []
+
+        if proxy_msg_group:
+            result.append("<proxy>")
+
+        for i, msg in enumerate(self._messages):
+            if msg.kind == last_msg:
+                prefix = ""
+            elif msg.kind != MessageKind.CHAT:
+                prefix = "<proxy>" if not proxy_msg_group else ""
+                proxy_msg_group = True
+            elif proxy_msg_group:
+                prefix = "</proxy>"
+                proxy_msg_group = False
+            else:
+                prefix = ""
+
+            if msg.kind == MessageKind.ERROR:
+                if self._wrap_errors or proxy_msg_group:
+                    text = f"PROXY ERROR {msg.status_code}: {msg.text}"
+                    text = f"Error {msg.status_code}: {msg.text}\n"
+                else:
+                    text = msg.text
+            elif i != len(self._messages) - 1:  # Is this last msg
+                text = f"{msg.text}\n"
+            else:
+                text = msg.text
+
+            result.append(f"{prefix}{text}")
+
+            # TODO: Invariant: result[-1] always ends with a \n before last msg
+
+            last_msg = msg.kind
+
+        if proxy_msg_group:
+            if not result[-1].endswith("\n"):
+                result.append("\n")
+            result.append("</proxy>")
+
+        return "".join(result)
+
+    @property
+    def status(self) -> int:
+        return self._status
+
+    @property
+    def status_code(self) -> int:
+        click.echo("!!! Don't use .status_code on a ResponseHelper!!!")
+        return self.build().status_code
+
+    @property
+    def response(self) -> int:
+        click.echo("!!! Don't use .response on a ResponseHelper!!!")
+        return self.build().response
+
 
 ################################################################################
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from gfjproxy.utils import is_proxy_test
+from gfjproxy.utils import ResponseHelper, is_proxy_test
 
 ################################################################################
 
@@ -32,6 +32,193 @@ def test_is_proxy_test():
     assert is_proxy_test(SAMPLE_PROXY_TEST_JSON)
 
     assert not is_proxy_test(SAMPLE_NORMAL_CHAT_JSON)
+
+
+################################################################################
+
+
+def test_response_helper():
+    """Usage test."""
+
+    def make_jai_res_str(*content):
+        content = repr("\n".join(content))[1:-1]
+        return [
+            '{"choices": [{"index": 0, "message": {"role": "assistant", "content": "'
+            + content
+            + '"}, "finish_reason": "stop"}]}'
+        ]
+
+    # Basic error responses (Google AI or something returned an error)
+    # ----------------------------------------------------------------
+
+    assert (  # Ensure the status_code is preserved
+        ResponseHelper().build_error("Lorem Ipsum", 400).status_code == 400
+    )
+
+    assert (  # Chat response error (JanitorAI will prepend "PROXY ERROR")
+        ResponseHelper().build_error("Lorem Ipsum", 400).response == ["Lorem Ipsum"]
+    )
+
+    assert (  # Proxy test error (JanitorAI shows this error message unaltered)
+        ResponseHelper(wrap_errors=True)
+        .build_error("Lorem Ipsum", 400)
+        .response  # flask.Response
+        == ['{"error": "PROXY ERROR 400: Lorem Ipsum"}']
+    )
+
+    # Basic chat responses (successful responses from Google AI)
+    # ----------------------------------------------------------
+
+    assert (  # Ensure the status_code is 200 for JanitorAI to accept the message
+        ResponseHelper().build_message("Hello, World!").status_code == 200
+    )
+
+    assert (  # Ensure the model's output is unaltered in this case
+        ResponseHelper().add_message("Hello, World!").message == "Hello, World!"
+    )
+
+    assert (  # Don't strip white space in this case
+        ResponseHelper().add_message("  \nZXY  \n").message == "  \nZXY  \n"
+    )
+
+    assert (  # Ensure the result is in the valid JanitorAI response format
+        ResponseHelper().build_message("abcdef").response == make_jai_res_str("abcdef")
+    )
+
+    assert (  # wrap_errors should NOT have an effect on this
+        ResponseHelper(wrap_errors=True).build_message("abcdef").response
+        == make_jai_res_str("abcdef")
+    )
+
+    assert (  # Ensure support for joining multiple bot messages together
+        ResponseHelper().add_message("Hello, World!", "Lorem Ipsum").message
+        == "Hello, World!\nLorem Ipsum"
+    )
+
+    assert (  # Multiple bot messages should not affect the final result
+        ResponseHelper()
+        .build_message("Hello, World!", "Lorem Ipsum")
+        .response  # flask.Response
+        == make_jai_res_str("Hello, World!", "Lorem Ipsum")
+    )
+
+    # Basic proxy chat responses (non-inline command output from proxy)
+    # -----------------------------------------------------------------
+
+    assert (  # Ensure the status_code is 200 for JanitorAI to accept the message
+        ResponseHelper().add_proxy_message("Hello, World!").build().status_code == 200
+    )
+
+    assert (  # Ensure a message is wrapped within <proxy> tags
+        ResponseHelper().add_proxy_message("Hello, World!").message
+        == "<proxy>Hello, World!\n</proxy>"
+    )
+
+    assert (  # Join multiple proxy messages together
+        ResponseHelper()
+        .add_proxy_message("abc")
+        .add_proxy_message("def")
+        .add_proxy_message("ghi")
+        .message
+        == "<proxy>abc\ndef\nghi\n</proxy>"
+    )
+
+    assert (  # Ensure the result is in the valid JanitorAI response format
+        ResponseHelper()
+        .add_proxy_message("abc")
+        .add_proxy_message("def")
+        .add_proxy_message("ghi")
+        .build()
+        .response  # flask.Response
+        == make_jai_res_str("<proxy>abc", "def", "ghi", "</proxy>")
+    )
+
+    # Complex multi-line chat responses (command output and bot responses)
+    # --------------------------------------------------------------------
+
+    assert (  # Ensure a proxy message in prefix position works
+        ResponseHelper().add_proxy_message("abc").add_message("def").message
+        == "<proxy>abc\n</proxy>def"
+    )
+
+    assert (  # Ensure a proxy message in suffix position works
+        ResponseHelper().add_message("abc").add_proxy_message("def").message
+        == "abc\n<proxy>def\n</proxy>"
+    )
+
+    assert (  # Ensure a proxy message can be sandwiched between bot responses
+        ResponseHelper()
+        .add_message("abc")
+        .add_proxy_message("def")
+        .add_message("ghi")
+        .message
+        == "abc\n<proxy>def\n</proxy>ghi"
+    )
+
+    assert (  # Ensure proxy messages coalesce
+        ResponseHelper()
+        .add_message("abc")
+        .add_proxy_message("lorem")
+        .add_proxy_message("ipsum")
+        .add_message("def")
+        .message
+        == "abc\n<proxy>lorem\nipsum\n</proxy>def"
+    )
+
+    assert (  # Ensure proxy messages can be interleaved with bot responses
+        ResponseHelper()
+        .add_message("A")
+        .add_proxy_message("B")
+        .add_message("C")
+        .add_proxy_message("D")
+        .message
+        == "A\n<proxy>B\n</proxy>C\n<proxy>D\n</proxy>"
+    )
+
+    # Complex multi-line chat responses (chat, error and proxy)
+    # ---------------------------------------------------------
+
+    assert (  # Ensure errors are wrapped like proxy messages
+        ResponseHelper()
+        .add_message("abc")
+        .add_error("lol", 404)
+        .add_message("ghi")
+        .message
+        == "abc\n<proxy>Error 404: lol\n</proxy>ghi"
+    )
+
+    assert (  # Ensure errors and proxy coalesce together
+        ResponseHelper()
+        .add_message("abc")
+        .add_error("lol", 404)
+        .add_proxy_message("kek")
+        .add_message("ghi")
+        .message
+        == "abc\n<proxy>Error 404: lol\nkek\n</proxy>ghi"
+    )
+
+    assert (  # Test all features at once
+        ResponseHelper()
+        .add_message("  Bot reply with leading spaces")
+        .add_proxy_message("Proxy banner")
+        .add_error("Proxy had an error", 500)
+        .add_message("This comes after the closing tag")
+        .add_message("Bot reply with trailing spaces  ")
+        .add_proxy_message("More proxy banner")
+        .add_error("Proxy had another error", 502)
+        .build()
+        .response  # flask.Response
+        == make_jai_res_str(
+            "  Bot reply with leading spaces",
+            "<proxy>Proxy banner",
+            "Error 500: Proxy had an error",
+            "</proxy>This comes after the closing tag",
+            "Bot reply with trailing spaces  ",
+            "<proxy>More proxy banner",
+            "Error 502: Proxy had another error",
+            "</proxy>",
+        )
+    )
 
 
 ################################################################################
