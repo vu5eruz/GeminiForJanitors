13ee7babaec3717393fbb70cef183d9a59c13849
vu5eruz <undefined@disroot.org>
2025-09-25 17:12:37 -0400

    Use git to get the date directly

--- gfjproxy/_globals.py
+++ gfjproxy/_globals.py
@@ -4,28 +4,26 @@ from os import environ as _env
 from os import scandir as _scandir
 
 
-# Based on https://github.com/numpy/numpy/blob/c6169853b871411ef207db1987a27f134a89b1cb/numpy/_build_utils/gitversion.py#L21
-def _append_git_version(version):
+def _make_git_version():
     import os.path
     import subprocess
 
+    version = "unknown"
+
     try:
         p = subprocess.Popen(
-            ["git", "log", "-1", "--format=%H"],
+            ["git", "log", "-1", "--date=format:%Y.%m.%d", "--format=%ad-%h"],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             cwd=os.path.dirname(__file__),
         )
-    except FileNotFoundError:
+    except Exception:
         pass
     else:
         out, err = p.communicate()
 
         if p.returncode == 0:
-            git_hash = out.decode("utf-8").strip()
-
-            if git_hash:
-                version += f"-{git_hash[:7]}"
+            version = out.decode().strip()
 
     return version
 
@@ -79,7 +77,7 @@ PROXY_AUTHORS = [
 
 PROXY_NAME = "GeminiForJanitors"
 
-PROXY_VERSION = _append_git_version("2025.09.25")
+PROXY_VERSION = _make_git_version()
 
 REDIS_URL = _env.get("GFJPROXY_REDIS_URL")
 
