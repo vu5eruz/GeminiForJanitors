9c3d3c2d572a08a8e1117700803e2b064777302c
vu5eruz <undefined@disroot.org>
2025-11-30 11:32:23 -0400

    Handle connection error with Redis user storage

--- gfjproxy/storage.py
+++ gfjproxy/storage.py
@@ -1,10 +1,16 @@
+import redis.exceptions
+
 from ._globals import DEVELOPMENT, REDIS_URL
 from .xuiduser import LocalUserStorage, RedisUserStorage
 
-
 if REDIS_URL is not None:
-    storage = RedisUserStorage(REDIS_URL)
-    print(" * Using Redis user storage")
+    try:
+        storage = RedisUserStorage(REDIS_URL)
+        print(" * Using Redis user storage")
+    except redis.exceptions.ConnectionError as e:
+        print(" * ERROR: Could not connect to Redis")
+        print(e)
+        exit(1)
 elif DEVELOPMENT:
     storage = LocalUserStorage()
     print(" * WARNING: Using local user storage")
